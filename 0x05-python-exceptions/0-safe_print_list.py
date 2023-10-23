6123de639a2f8601a371832a94028925a9f7ff92
Sylviachinny.sylvia <Sylviachinny.chinyeresylvia222@gmail.com>
2023-10-23 05:05:07 -0700

    safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
+    """Safely print the first x elements of a list
+    Args:
+        my_list (list): The list containing elements to be printed.
+        x (int): The number of elements to print.
+
+    Returns:
+        int: The real number of elements printed
+    """    
     try:
         for index in range(x):
-            print(my_list[index], end=' '):
+            print(my_list[index], end=' ')
     except IndexError:
         pass
 
