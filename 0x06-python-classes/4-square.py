6d952dedf29c59aa879a0e6d3e36f165d20e21b1
Sylviachinny.sylvia <Sylviachinny.chinyeresylvia222@gmail.com>
2023-10-25 04:52:07 -0700

    4-square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a Square."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square.
+
+        Args:
+        size(int): The size of the new square.
+        """
+        self.size = size
+
+        @property
+        def size(self):
+            """Get the current size of the square."""
+            return (self.__size)
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size * self.__size)
